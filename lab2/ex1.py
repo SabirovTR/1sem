7de16a9854a7649a2c4bc2ec0bd0c86011389f2a
SabirovTR <71315309+SabirovTR@users.noreply.github.com>
2020-09-15 11:27:58 +0300

    Add files via upload

--- lab2/ex1.py
+++ lab2/ex1.py
@@ -1,1 +1,11 @@
+import random as rd
+import turtle as t
 
+t.shape('turtle')
+t.speed(0)
+
+for i in range(1000):
+    alpha = rd.random()*720
+    step_lenth = rd.randint(10, 30)
+    t.forward(step_lenth)
+    t.left(alpha)
